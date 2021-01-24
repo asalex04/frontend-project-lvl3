a1981b8ab948674114bc10a996ad411307ab6a86
asalex04 <arc04@mail.ru>
2021-01-24 15:00:51 +0400

    modified:   package-lock.json 	modified:   package.json 	new file:   src/index.html 	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,10 @@ module.exports = {
   entry: './src/index.js',
   module: {
     rules: [
-      //{ test: /\.svg$/, use: 'svg-inline-loader' },
       { 
         test: /\.css$/, 
         use: [ 'style-loader', 'css-loader' ] 
       },
-      // { test: /\.(js)$/, use: 'babel-loader' }
     ]
   },
  
